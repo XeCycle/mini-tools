fdc90cd5dfede41dca29a7fc4f3f65be50606c8d
Carl Lei <me@xecycle.info>
2022-01-17 14:03:32 +0800

    change syscall handling and update size in README

--- minisys.c
+++ minisys.c
@@ -1,16 +1,92 @@
 #include "minisys.h"
 #include <asm/unistd.h>
 
-long pause(void)
+static long syscall0(long sysnr)
 {
   long ret;
   asm volatile (
     "syscall"
     : "=a" (ret)
-    : "0" (__NR_pause)
-    : "rcx", "r11", "memory"
-    );
+    : "0" (sysnr)
+    : "rcx", "r11", "memory");
   return ret;
+}
+
+static long syscall1(long sysnr, long arg1)
+{
+  long ret;
+  asm volatile (
+    "syscall"
+    : "=a" (ret)
+    : "0" (sysnr), "D" (arg1)
+    : "rcx", "r11", "memory");
+  return ret;
+}
+
+static long syscall2(long sysnr, long arg1, long arg2)
+{
+  long ret;
+  asm volatile (
+    "syscall"
+    : "=a" (ret)
+    : "0" (sysnr), "D" (arg1), "S" (arg2)
+    : "rcx", "r11", "memory");
+  return ret;
+}
+
+static long syscall3(long sysnr, long arg1, long arg2, long arg3)
+{
+  long ret;
+  asm volatile (
+    "syscall"
+    : "=a" (ret)
+    : "0" (sysnr), "D" (arg1), "S" (arg2), "d" (arg3)
+    : "rcx", "r11", "memory");
+  return ret;
+}
+
+static long syscall4(long sysnr, long arg1, long arg2, long arg3, long arg4)
+{
+  long ret;
+  register long r10 asm("r10") = arg4;
+  asm volatile (
+    "syscall"
+    : "=a" (ret)
+    : "0" (sysnr), "D" (arg1), "S" (arg2), "d" (arg3), "r" (r10)
+    : "rcx", "r11", "memory");
+  return ret;
+}
+
+static long syscall5(long sysnr, long arg1, long arg2, long arg3, long arg4, long arg5)
+{
+  long ret;
+  register long r10 asm("r10") = arg4;
+  register long r8 asm("r8") = arg5;
+  asm volatile (
+    "syscall"
+    : "=a" (ret)
+    : "0" (sysnr), "D" (arg1), "S" (arg2), "d" (arg3), "r" (r10), "r" (r8)
+    : "rcx", "r11", "memory");
+  return ret;
+}
+
+static long syscall6(long sysnr, long arg1, long arg2, long arg3, long arg4, long arg5, long arg6)
+{
+  long ret;
+  register long r10 asm("r10") = arg4;
+  register long r8 asm("r8") = arg5;
+  register long r9 asm("r9") = arg6;
+  asm volatile (
+    "syscall"
+    : "=a" (ret)
+    : "0" (sysnr), "D" (arg1), "S" (arg2), "d" (arg3), "r" (r10), "r" (r8), "r" (r9)
+    : "rcx", "r11", "memory");
+  return ret;
+}
+
+long pause(void)
+{
+  return syscall0(__NR_pause);
 }
 
 __attribute__((naked, noreturn))
@@ -30,30 +106,11 @@ long sigaction(int signum,
 {
   act->sa_flags |= SA_RESTORER;
   act->sa_restorer = &signal_restorer;
-  long ret;
-  asm volatile (
-    "mov %0, %%r10"
-    :
-    : "i" (sizeof(sigset_t))
-    : "r10");
-  asm volatile (
-    "syscall"
-    : "=a" (ret)
-    : "0" (__NR_rt_sigaction), "D" (signum), "S" (act), "d" (oldact)
-    : "rcx", "r11", "memory"
-    );
-  return ret;
+  return syscall4(__NR_rt_sigaction, signum, (long)act, (long)oldact, sizeof(sigset_t));
 }
 
 __attribute__((noreturn))
 void _exit(int exit_status)
 {
-  long ret;
-  asm volatile (
-    "syscall"
-    : "=a" (ret)
-    : "0" (__NR_exit_group), "D" (exit_status)
-    : "rcx", "r11", "memory"
-    );
-  __builtin_unreachable();
+  syscall1(__NR_exit_group, exit_status);
 }
